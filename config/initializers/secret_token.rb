487eb849079dfa709413d6815286887e4b7a9c33
Michal Bartkiewicz <michal.bartkiewicz@gmail.com>
2011-05-31 17:41:35 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plejsbook::Application.config.secret_token = 'dc72a7e5cc074e17effd4d7fa38fa1c40ccfa5d0858d89162ef456359efdf54ee6878aa1d380ce58d391d02dcb49692902bbed3c5c9eb7890d1cfdfa76993f41'
