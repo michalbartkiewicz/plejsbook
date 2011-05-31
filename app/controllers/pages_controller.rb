948a3eeb60907f1431b083e2e61c3105229a64e0
Michal Bartkiewicz <michal.bartkiewicz@gmail.com>
2011-05-31 18:44:23 +0200

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Strona glowna"
   end
 
   def contact
+    @title = "Kontakt"
   end
+  
+  def about
+    @title = "O nas"
+  end  
 
 end
